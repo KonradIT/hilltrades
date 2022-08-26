28ab5543688c5db489bf636d66b78119d319a064
Scott Schmidt <s@scottschmidt.io>
2022-08-26 14:57:18 -0400

    Reorganize main.py so that it can be run without pushbullet and prepare it to interact with new CapitolTrades class

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-from capitoltrades import CapitolTrades, PoliticalParty, CongressType
-from pprint import pprint
-from pushbullet import Pushbullet
-import os
 from dateutil.parser import parse
-from datetime import datetime as dt
-import datetime
-import logging
+from datetime import datetime as dt, timedelta
 import dotenv
-import time
+import logging
+import os
+from typing import Any, Dict, Optional, Tuple
 
-logging.basicConfig(handlers=[
-    logging.FileHandler("trades.log"),
-    logging.StreamHandler()
-],
-    format="%(asctime)s %(message)s",
-    datefmt="%H:%M:%S",
-    level=logging.DEBUG)
+from pushbullet import Pushbullet
 
-dotenv.load_dotenv(".env")
-
-assert os.getenv("PUSHBULLET_API_KEY") is not None
-pb = Pushbullet(os.getenv("PUSHBULLET_API_KEY"))
-capitol = CapitolTrades()
-
+from capitoltrades import CapitolTrades
 
 class Parsers:
 
@@ -38,29 +23,80 @@ class Parsers:
         if shareprice == None:
             return ""
         return "x $%s" % shareprice
+    
+    @staticmethod
+    def presentable_trade(tx: Dict[str, Any], name: str) -> Tuple[str, str]:
+        """Creates a presentable trade which can be consumed by the Pushbullet API."""
+        title = "New 💰 by %s" % name
+        body = """🏢: %s
+        💰: %s
+        📅: %s
+        💸: %s
+        #️⃣: %s %s
+        💬: %s""" % (
+            tx.get("asset", {}).get("assetTicker"),
+            Parsers.tx_date(tx.get("txDate")), 
+            tx.get("txType"), 
+            tx.get("value"), 
+            tx.get("size"), 
+            Parsers.share_price(tx.get("price")),
+            tx.get("comment")
+        )
+        return title, body
 
 
-nancy = capitol.get_politician_id("Pelosi")
-p = capitol.trades(
-    politicians=[nancy],
-    politicianParty=PoliticalParty.BOTH,
-    congressType=CongressType.REPRESENTATIVE,
-    ticker=True
-)
+def _init_pb() -> Optional[Pushbullet]:
+    api_key = os.getenv("PUSHBULLET_API_KEY")
+    if not api_key:
+        return None
+    else:
+        return Pushbullet(api_key)
 
-for tx in p:
-    print(tx)
-    if parse(tx.get("addedDate")).date() + datetime.timedelta(days=1) == dt.now().date() or \
-        parse(tx.get("addedDate")).date() + datetime.timedelta(days=2) == dt.now().date() or \
-        parse(tx.get("addedDate")).date()  == dt.now().date():
-        logging.info(str(tx))
+def main(
+    capitol: CapitolTrades,
+    pb: Optional[Pushbullet],
+    check_dates: bool = True,
+    name: str = "Pelosi",
+):
+    """A quick script to grab all of a politician's trades.
+    If check_date = True, only displays trades within the last three days,
+    otherwise displays all trades."""
+    nancy_id = capitol.get_politician_id(name)
+    p = capitol.trades(nancy_id)
 
-        pb.push_note("New 💰 by Nancy Pelosi", """🏢: %s
-💰: %s
-📅: %s
-💸: %s
-#️⃣: %s %s
-💬: %s""" % (tx.get("ticker"), Parsers.tx_date(tx.get("transactionDate")), tx.get("tradeType"), tx.get("tradeValueRange"), tx.get("shares"), Parsers.share_price(tx.get("sharePrice")), tx.get("comment")))
-        time.sleep(2)
+    for tx in p:
+        if not tx:
+            continue
 
-print("Done fetching values.")
+        if check_dates:
+            if any(
+                date_checker(tx["pubDate"]) for date_checker in [
+                    lambda d: parse(d).date() + timedelta(days=1) == dt.now().date(),
+                    lambda d: parse(d).date() + timedelta(days=2) == dt.now().date(),
+                    lambda d: parse(d).date()  == dt.now().date()
+                ]
+            ):
+                title, body = Parsers.presentable_trade(tx, name)
+                print(title, body, "\n") if not pb else pb.push_note(title, body)
+        else:
+            title, body = Parsers.presentable_trade(tx, name)
+            print(title, body, "\n") if not pb else pb.push_note(title, body)
+        
+    print("Done fetching values.")
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        handlers=[
+            logging.FileHandler("trades.log"),
+            logging.StreamHandler()
+        ],
+        format="%(asctime)s %(message)s",
+        datefmt="%H:%M:%S",
+        level=logging.DEBUG
+    )
+
+    dotenv.load_dotenv(".env")
+
+    capitol = CapitolTrades()
+    pb = _init_pb()
+    main(capitol, pb)
