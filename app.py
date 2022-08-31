8655211befaf7c4c65906fb987de3e9814c32b0d
Scott Schmidt <s@scottschmidt.io>
2022-08-30 21:43:35 -0400

    add flask to run standalone API server based on CapitolTrades model

---
+++ app.py
@@ -0,0 +1,40 @@
+import flask
+
+from capitoltrades import CapitolTrades
+
+app = flask.Flask(__name__)
+
+capitol = CapitolTrades()
+
+
+@app.route("/get-pids")
+def get_pids():
+    """See the map of all the known politicians mapped to their IDs for capitoltrades.com.
+    Can be used to query the `by_pid` endpoint."""
+    return flask.jsonify(capitol.politicians)
+
+@app.route("/by-name/<name>", methods=["GET"])
+def by_name(name):
+    """Query all of the politicians trades by their name. Returns the first match to the given name. Not the most accurate,
+    so we suggest finding the ID with `get_pids()` first, then querying the `by_pid()` endpoint."""
+    pid = capitol.get_politician_id(name)
+    if not pid:
+        return flask.jsonify({
+            "status": "error",
+            "message": "No match found for search term {}.".format(name)
+        })
+    else:
+        return by_pid(pid)
+
+@app.route("/by-pid/<pid>", methods=["GET"])
+def by_pid(pid):
+    """Query all of the politicians trades by their politician ID assigned by capitoltrades.com"""
+    try:
+        trades = capitol.trades(pid)
+    except AssertionError:
+        return flask.jsonify({
+            "status": "error",
+            "message": "No match found for the provided politician ID of {}.".format(pid),
+        })
+    
+    return flask.jsonify(trades)
