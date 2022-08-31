82ea0e03a6bc7cc2385a49f1f45a407873f3955e
Scott Schmidt <s@scottschmidt.io>
2022-08-30 21:48:00 -0400

    init fresh CapitolTrades for each endpoint; don't want the same instance running for eternity

--- app.py
+++ app.py
@@ -4,19 +4,18 @@ from capitoltrades import CapitolTrades
 
 app = flask.Flask(__name__)
 
-capitol = CapitolTrades()
-
-
 @app.route("/get-pids")
 def get_pids():
     """See the map of all the known politicians mapped to their IDs for capitoltrades.com.
     Can be used to query the `by_pid` endpoint."""
+    capitol = CapitolTrades()
     return flask.jsonify(capitol.politicians)
 
 @app.route("/by-name/<name>", methods=["GET"])
 def by_name(name):
     """Query all of the politicians trades by their name. Returns the first match to the given name. Not the most accurate,
     so we suggest finding the ID with `get_pids()` first, then querying the `by_pid()` endpoint."""
+    capitol = CapitolTrades()
     pid = capitol.get_politician_id(name)
     if not pid:
         return flask.jsonify({
@@ -29,6 +28,7 @@ def by_name(name):
 @app.route("/by-pid/<pid>", methods=["GET"])
 def by_pid(pid):
     """Query all of the politicians trades by their politician ID assigned by capitoltrades.com"""
+    capitol = CapitolTrades()
     try:
         trades = capitol.trades(pid)
     except AssertionError:
